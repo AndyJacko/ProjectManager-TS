685f1e3ece01c56f49c029b2a1231c75b52bfcb2
Andy Jacko <wisdom.mcr@gmail.com>
2022-12-02 11:02:53 +0000

    Render ProjectItems with a class...

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-// Project Type
+// Project Type Class
 enum ProjectStatus {
   Active,
   Finished,
@@ -14,7 +14,7 @@ class Project {
   ) {}
 }
 
-// Project State Management Class
+// ProjectState Management Class
 type Listener<T> = (items: T[]) => void;
 
 class State<T> {
@@ -91,7 +91,7 @@ function validate(validatableInput: Validatable) {
   return isValid;
 }
 
-// autobind decorator
+// Autobind Decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
@@ -135,6 +135,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.project.people.toString();
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -174,10 +196,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     listEl.innerHTML = "";
 
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
     }
   }
 }
